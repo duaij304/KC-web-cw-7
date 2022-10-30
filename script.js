f77cbdb2e7027e2f06d0ef8a86952c12e4d92bd2
duaij304 <69263437+duaij304@users.noreply.github.com>
2022-10-30 15:39:10 +0300

    cw is done

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 
+function BMI(weight,height){
+    let result= weight / (height * height);
+    return result;
+}
+
+console.log(BMI(80,1.63))
+
+function Status(Bodymass)
+{
+    if(Bodymass<18)
+    {
+      return "لديك نقص في الوزن"
+    }
+    else if(Bodymass<25&&Bodymass>18.5)
+    {
+       return "وزنك صحي"
+    }
+     else if(Bodymass>25)
+     {
+       return "لديك زيادة في الوزن" 
+       
+     }
+}
+function Calculate()
+{
+    let weight = document.getElementById("weight").value
+     let height = document.getElementById("height").value
+      let bmi_value= BMI(weight,height);
+        let desc = Status(bmi_value);
+          let box =document.getElementById("result");
+            box.innerText=bmi_value+"=="+desc;
+}
+
